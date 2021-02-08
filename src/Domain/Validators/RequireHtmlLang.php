0ee14e32ea0cea42d46bebda0cc31ae449959b3f
Vincent Hagen <blackshadev@users.noreply.github.com>
2021-02-08 20:50:32 +0100

    Validator for lang attribute on html tag (#1)
    * feat(validator-lang-attr) Validator for lang attribute on html tag

---
+++ src/Domain/Validators/RequireHtmlLang.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JeroenG\Hummingbird\Domain\Validators;
+
+use JeroenG\Hummingbird\Application\ParserInterface;
+use JeroenG\Hummingbird\Domain\Element;
+
+class RequireHtmlLang implements ValidatorInterface
+{
+    public const REASON_NO_HTML_TAG = 1;
+
+    public const REASON_TOO_MANY_HTML_TAGS = 2;
+
+    public const REASON_NO_LANG_ATTR = 3;
+
+    public const REASON_INVALID_LANG = 4;
+
+    private int $reason = 0;
+
+    public function validate(ParserInterface $parser): bool
+    {
+        $elements = $parser->find('html')->getElements();
+        if (0 === count($elements)) {
+            $this->reason = self::REASON_NO_HTML_TAG;
+            return false;
+        }
+
+        if (1 < count($elements)) {
+            $this->reason = self::REASON_TOO_MANY_HTML_TAGS;
+            return false;
+        }
+
+        /** @var Element $htmlElement */
+        $htmlElement = $elements[0];
+        $langAttr = $htmlElement->getAttribute('lang');
+
+        if (null === $langAttr) {
+            $this->reason = self::REASON_NO_LANG_ATTR;
+            return false;
+        }
+
+        if (!self::isValidLangCode($langAttr)) {
+            $this->reason = self::REASON_INVALID_LANG;
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getSubject(): string
+    {
+        return 'There should be only one <html> tag with an should have a valid lang attribute.';
+    }
+
+    public function getErrorMessage(): string
+    {
+        return match ($this->reason) {
+            self::REASON_NO_HTML_TAG => 'No <html> tag found',
+            self::REASON_TOO_MANY_HTML_TAGS => 'Multiple <html> tags found',
+            self::REASON_NO_LANG_ATTR => 'No lang attribute found on the <html> tag.',
+            self::REASON_INVALID_LANG => 'Invalid lang attribute. It should be an ISO language code of length 2.',
+        };
+    }
+
+    private static function isValidLangCode(string $code): bool
+    {
+        return mb_strlen($code) === 2;
+    }
+}
---
+++ tests/Integration/Validators/RequireHtmlLangTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JeroenG\Hummingbird\Tests\Integration\Validators;
+
+use JeroenG\Hummingbird\Domain\Validators\RequireHtmlLang;
+use JeroenG\Hummingbird\Tests\Support\WithCollector;
+use PHPUnit\Framework\TestCase;
+
+class RequireHtmlLangTest extends TestCase
+{
+    use WithCollector;
+
+    private RequireHtmlLang $validator;
+
+    protected function setUp(): void
+    {
+        $this->validator = new RequireHtmlLang();
+    }
+
+    public function test_it_passes_on_valid_document()
+    {
+        $parser = $this->collector()->collect('<html lang="nl"></html>');
+        $result = $this->validator->validate($parser);
+
+        self::assertTrue($result);
+    }
+
+    public function test_it_fails_on_missing_html_tag(): void
+    {
+        $parser = $this->collector()->collect('<head></head><body></body>');
+        $result = $this->validator->validate($parser);
+
+        self::assertFalse($result);
+        self::assertStringContainsStringIgnoringCase('no <html>', $this->validator->getErrorMessage());
+    }
+
+    public function test_it_fails_on_multiple_html_tags(): void
+    {
+        $parser = $this->collector()->collect('<html></html><html></html>');
+        $result = $this->validator->validate($parser);
+
+        self::assertFalse($result);
+        self::assertStringContainsStringIgnoringCase('multiple <html> tags', $this->validator->getErrorMessage());
+    }
+
+    public function test_it_fails_on_no_lang_attr(): void
+    {
+        $parser = $this->collector()->collect('<html><head><body></body></head></html>');
+        $result = $this->validator->validate($parser);
+
+        self::assertFalse($result);
+        self::assertStringContainsStringIgnoringCase('no lang attr', $this->validator->getErrorMessage());
+    }
+
+    public function test_it_fails_on_empty_lang_attr(): void
+    {
+        $parser = $this->collector()->collect('<html lang=""><head><body></body></head></html>');
+        $result = $this->validator->validate($parser);
+
+        self::assertFalse($result);
+        self::assertStringContainsStringIgnoringCase('invalid lang attr', $this->validator->getErrorMessage());
+    }
+
+    public function test_it_fails_on_invalid_lang_attr(): void
+    {
+        $parser = $this->collector()->collect('<html lang="nederland"><head><body></body></head></html>');
+        $result = $this->validator->validate($parser);
+
+        self::assertFalse($result);
+        self::assertStringContainsStringIgnoringCase('invalid lang attr', $this->validator->getErrorMessage());
+    }
+}
